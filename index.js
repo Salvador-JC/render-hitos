e2e9271aff7339cefc4289c78bb078d3e66a1589
Salvador-JC <salvadorjimenezciborro@gmail.com>
2026-01-07 09:57:26 +0100

    Minireto hito1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Render - Primera versión");
+  res.send("Render - Version actualizada para el minireto");
 });
 
 app.listen(PORT, () => {
