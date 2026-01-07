23088073018f2ad5ec141bbbf69854ee9519e4b2
Salvador-JC <salvadorjimenezciborro@gmail.com>
2026-01-07 10:28:49 +0100

    Hito3 fallo a proposito

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 
+// ❌ MÓDULO INEXISTENTE
+const fakeLib = require("this-library-does-not-exist");
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Render - Hito-2");
+  res.send("Esta versión está rota");
 });
-
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
